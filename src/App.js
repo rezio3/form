fb77c0f31160718bb18d06020fb14a30c178f0db
rezio3 <jakub.rezler96@gmail.com>
2024-06-26 23:58:26 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import Form from "./components/Form.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <div className="App d-flex justify-content-center align-items-center">
+        <Form />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form.tsx
@@ -0,0 +1,7 @@
+import "../style/form.scss";
+
+const Form = () => {
+  return <div className="w-50 h-50 form-container"></div>;
+};
+
+export default Form;
